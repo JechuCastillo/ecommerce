7c2e673f5478b51bfd1c8657779105eee296a81f
Jesus <jesus.ecastillo25@gmail.com>
2025-06-08 19:55:00 -0300

    Añadiendo front a crear producto con admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,2 +1,25 @@
-const formProducto = document.querySelector('#crear-producto');
-console.log(formProducto)
+const formProducto = document.querySelector("#crear-producto");
+formProducto.addEventListener("submit", crearProducto);
+
+async function crearProducto(e) {
+  e.preventDefault();
+  const nombre = document.querySelector("#nombre-product").value;
+  const precio = document.querySelector("#precio-product").value;
+  const stock = document.querySelector("#stock-product").value;
+  const resultado = document.querySelector(".error-producto");
+  try {
+    const res = await fetch("/api/productos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ nombre, precio, stock }),
+    });
+    const data = await res.json();
+    console.log(data);
+    if (!res.ok) {
+      throw new Error(data.error || "Error al crear el producto");
+    }
+    resultado.innerHTML = `<p>Producto agregado</p>`;
+  } catch (err) {
+    resultado.innerHTML = `<p>Error al agregar producto</p>`;
+  }
+}
